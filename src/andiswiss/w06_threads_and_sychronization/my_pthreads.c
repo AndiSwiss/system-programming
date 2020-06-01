8e118d08da716663790901e19920b910b5502e18
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-06-01 18:12:30 +0200

    extended my_pthreads - passing multiple values via a struct

--- src/andiswiss/w06_threads_and_sychronization/my_pthreads.c
+++ src/andiswiss/w06_threads_and_sychronization/my_pthreads.c
@@ -5,21 +5,34 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void *someWork(void *threadName) {
+typedef struct Arguments {
+    char *name;
+    int astronauts;
+    double flightTime;
+} Arguments;
+
+
+
+void *someWork(void *args) {
     sleep(1);
-    char * name = (char *) threadName;
-    printf("This is some work done by thread named '%s'. My thread id is: %ld.\n", name, (long) pthread_self());
+    Arguments *a = (Arguments *) args;
+    printf("Welcome earth! I'm space craft '%s'. I brought %d astronauts to the ISS in %f hours. "
+           "Most likely, parts of my software was written in C or is based on C! My thread-id is %ld.\n",
+           a->name, a->astronauts, a->flightTime, (long) pthread_self());
     pthread_exit(NULL);
 }
 
 int main(void) {
-
     pthread_t thread;
 
-    char *name = "Endeavor";
-    printf("Hello, I am thread #%ld, creating a new thread named '%s'.\n", (long) pthread_self(), name);
+    Arguments args;
+    args.name = "Endeavour";
+    args.astronauts = 2;
+    args.flightTime = 18.91;
 
-    int answer = pthread_create(&thread, NULL, someWork, (void *) name);
+    printf("Hello, I am thread #%ld, launching the first manned falcon 9 into space on May 30th 2020...\n", (long) pthread_self());
+
+    int answer = pthread_create(&thread, NULL, someWork, (void *) &args);
     if (answer) {
         // NOTE: according to https://www.man7.org/linux/man-pages/man3/pthread_create.3.html
         // pthread_create(...) returns 0 when successful
