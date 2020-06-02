317f7dca7922415fdf7e0dc55a44dc1f2cfd7612
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-06-02 15:59:22 +0200

    works, but differs quite a bit from the sample solution

--- src/andiswiss/w08_ipc_with_pipes/my_pipe.c
+++ src/andiswiss/w08_ipc_with_pipes/my_pipe.c
@@ -10,7 +10,10 @@
 
 
 int main(void) {
-    int pfd[2];  // Array with the two pipe-file-descriptors
+    // Array with the two pipe-file-descriptors
+    // pdf[0]: for reading (the receiver)
+    // pdf[1]: for writing (the sender)
+    int pfd[2];
 
     // Create the pipe:
     if (pipe(pfd) == -1) {
@@ -25,17 +28,19 @@ int main(void) {
         case 0:
             // Child, reading data:
             printf("I'm the child with pid %d.\n", getpid());
-            char *message = "hello";
+            char message[] = "hello";
+            // introduce sleep, to ensure, that the parent is in fact waiting for the child:
+            sleep(1);
             write(pfd[1], message, 5);
-//            close(pfd[0]);
+            close(pfd[0]);
             exit(0);
         default:
             // Parent:
             printf("I'm the parent with pid %d.\n", getpid());
-            char *received = "";
+            char received[5];
             read(pfd[0], received, 5);
             printf("Got the following message from child: %s\n", received);
-//            close(pfd[1]);
+            close(pfd[1]);
             exit(0);
     }
 
