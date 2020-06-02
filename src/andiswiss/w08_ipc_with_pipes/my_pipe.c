9271ac7a6818b4c6bb2d7c47fe2a430042089c52
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-06-02 16:08:28 +0200

    optimized my_pipe.c

--- src/andiswiss/w08_ipc_with_pipes/my_pipe.c
+++ src/andiswiss/w08_ipc_with_pipes/my_pipe.c
@@ -9,6 +9,8 @@
 #define BUF_SIZE 10
 
 
+
+
 int main(void) {
     // Array with the two pipe-file-descriptors
     // pdf[0]: for reading (the receiver)
@@ -27,19 +29,34 @@ int main(void) {
             return errno;
         case 0:
             // Child, reading data:
-            printf("I'm the child with pid %d.\n", getpid());
+            close(pfd[0]);
             char message[] = "hello";
+            size_t size = sizeof(message) / sizeof(message[0]);
             // introduce sleep, to ensure, that the parent is in fact waiting for the child:
             sleep(1);
-            write(pfd[1], message, 5);
+            write(pfd[1], message, size);
             close(pfd[0]);
             exit(0);
         default:
             // Parent:
-            printf("I'm the parent with pid %d.\n", getpid());
-            char received[5];
-            read(pfd[0], received, 5);
-            printf("Got the following message from child: %s\n", received);
+            close(pfd[1]);
+
+            /// VERSION, IF YOU KNOW, how many bytes, you will expect:
+//            char received[5];
+//            read(pfd[0], received, 5);
+//            printf("\nGetting the following message from child: %s\n", received);
+
+
+            /// Dynamic version, if yo DON't know, how many bytes you would get (adapted from sample solution):
+            char title[] = "\nGetting the following message from child: ";
+            write(STDOUT_FILENO, title, sizeof(title) / sizeof(title[0]));
+            char received[1];
+            while (read(pfd[0], received, 1) == 1) {
+                write(STDOUT_FILENO, received, 1);
+            }
+            write(STDOUT_FILENO, "\n", 1);
+
+
             close(pfd[1]);
             exit(0);
     }
